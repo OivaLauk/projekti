182978c124f69fc9354d787b089be413997226d5
OivaLauk <oiva.laukkanen@edu.salpaus.fi>
2024-09-03 16:09:48 +0300

    Add files via upload

---
+++ projekti.js
@@ -0,0 +1,8 @@
+function dataCheck() {
+    document.getElementById(nimi)
+    document.getElementById(tieto)
+    if (nimi, tieto = null) {
+        document.getElementById(button).innerHTML = "Virhe! En lisännyt tietoa."
+    }
+
+}
