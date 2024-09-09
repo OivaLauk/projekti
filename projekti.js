6b3281d9f9fad8ebc2b58c696c3d160a90ba7a8b
OivaLauk <oiva.laukkanen@edu.salpaus.fi>
2024-09-09 12:57:23 +0300

    Ei valmis versio

--- projekti.js
+++ projekti.js
@@ -1,8 +1,26 @@
+const checkbox = document.getElementById("checkbox").checked;
+const person = document.getElementById("name").required = true;
+const data = document.getElementById("data").required = true;
+
+function infoCheck() {
+    if (form.value.length == 0);
+    const newDiv = document.createElement("div", id = "error");
+    const newContent = document.createTextNode("Virhe! En lisännyt tietoa");
+    newDiv.appendChild(newContent);
+    const currentDiv = document.getElementById("div2");
+    document.body.insertBefore(newDiv, currentDiv)
+    return console.log
+}
+
 function dataCheck() {
-    document.getElementById(nimi)
-    document.getElementById(tieto)
-    if (nimi, tieto = null) {
-        document.getElementById(button).innerHTML = "Virhe! En lisännyt tietoa."
-    }
 
 }
+
+function addElement1() {
+    const newDiv = document.createElement("div", id = "error");
+    const newContent = document.createTextNode("Virhe! En lisännyt tietoa");
+    newDiv.appendChild(newContent);
+    const currentDiv = document.getElementById("div2");
+    document.body.insertBefore(newDiv, currentDiv)
+}
+
